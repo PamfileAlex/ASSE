d66b9dda5c7edd57968ed4ceeba03b2af00c03c4
Pamfile Alex <damrosrak@gmail.com>
2023-01-07 13:40:42 +0200

    Added Serilog with Autofac integration

--- ASSE.Core/CoreModule.cs
+++ ASSE.Core/CoreModule.cs
@@ -1,9 +1,17 @@
 ﻿using Autofac;
+using AutofacSerilogIntegration;
+using Serilog;
 
 namespace ASSE.Core;
 public class CoreModule : Module
 {
 	protected override void Load(ContainerBuilder builder)
 	{
+		Log.Logger = new LoggerConfiguration()
+			.WriteTo.Console()
+			.WriteTo.File("log.txt")
+			.CreateLogger();
+
+		builder.RegisterLogger();
 	}
 }
