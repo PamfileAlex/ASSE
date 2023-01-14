b6ac79d044869ea5ea6a1975d4d99f210e5146de
Pamfile Alex <damrosrak@gmail.com>
2023-01-14 13:30:42 +0200

    Added UserRole validator and tests for it

---
+++ ASSE.DomainModel.Test/UserRoleValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using ASSE.DomainModel.Models;
+using ASSE.DomainModel.Validators;
+using FluentValidation.TestHelper;
+
+namespace ASSE.DomainModel.Test;
+public class UserRoleValidatorTests
+{
+	private readonly UserRoleValidator _validator;
+
+	public UserRoleValidatorTests()
+	{
+		_validator = new UserRoleValidator();
+	}
+
+	[Fact]
+	public void Validate_ValidUserRole_NoErrors()
+	{
+		var data = new UserRole(1, 1);
+
+		var result = _validator.TestValidate(data);
+
+		result.ShouldNotHaveAnyValidationErrors();
+	}
+
+	[Fact]
+	public void Validate_DefaultUserRole_Fails()
+	{
+		var data = new UserRole();
+
+		var result = _validator.TestValidate(data);
+
+		result.ShouldHaveAnyValidationError();
+	}
+
+	[Fact]
+	public void Validate_NoUserId_Fails()
+	{
+		var data = new UserRole()
+		{
+			RoleId = 1
+		};
+
+		var result = _validator.TestValidate(data);
+
+		result.ShouldHaveValidationErrorFor(x => x.UserId);
+	}
+
+	[Fact]
+	public void Validate_NoRoleId_Fails()
+	{
+		var data = new UserRole()
+		{
+			UserId = 1
+		};
+
+		var result = _validator.TestValidate(data);
+
+		result.ShouldHaveValidationErrorFor(x => x.RoleId);
+	}
+}
---
+++ ASSE.DomainModel/Validators/UserRoleValidator.cs
@@ -0,0 +1,12 @@
+﻿using ASSE.DomainModel.Models;
+using FluentValidation;
+
+namespace ASSE.DomainModel.Validators;
+public class UserRoleValidator : AbstractValidator<UserRole>
+{
+	public UserRoleValidator()
+	{
+		RuleFor(x => x.UserId).NotEmpty();
+		RuleFor(x => x.RoleId).NotEmpty();
+	}
+}
