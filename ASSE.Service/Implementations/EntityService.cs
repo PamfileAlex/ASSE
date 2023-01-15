9926b80d8644df96e0fcf6113db60745828c40cf
Pamfile Alex <damrosrak@gmail.com>
2023-01-15 13:42:00 +0200

    Added logging to services

--- ASSE.Service/Implementations/EntityService.cs
+++ ASSE.Service/Implementations/EntityService.cs
@@ -2,6 +2,7 @@
 using ASSE.DataMapper.Interfaces;
 using ASSE.Service.Interfaces;
 using FluentValidation;
+using Serilog;
 
 namespace ASSE.Service.Implementations;
 
@@ -18,7 +19,7 @@ public abstract class EntityService<T, D> : GenericService<D>, IEntityService<T>
 	where T : class, IKeyEntity, new()
 	where D : IDataAccess<T>
 {
-	private readonly IValidator<T> _validator;
+	protected readonly IValidator<T> _validator;
 
 	public EntityService(D dataAccess, IValidator<T> validator)
 		: base(dataAccess)
@@ -28,6 +29,7 @@ public abstract class EntityService<T, D> : GenericService<D>, IEntityService<T>
 
 	public virtual int Add(T data)
 	{
+		Log.Debug("Adding: {data}", data);
 		if (!ValidateAdd(data))
 		{
 			return default;
@@ -37,16 +39,19 @@ public abstract class EntityService<T, D> : GenericService<D>, IEntityService<T>
 
 	protected virtual bool ValidateAdd(T data)
 	{
+		Log.Debug("Validating add: {data}", data);
 		return _validator.Validate(data).IsValid;
 	}
 
 	public virtual T? Get(int id)
 	{
+		Log.Debug("Getting by id: {id}", id);
 		return _dataAccess.Get(id);
 	}
 
 	public virtual bool Update(T data)
 	{
+		Log.Debug("Updating: {data}", data);
 		if (!ValidateUpdate(data))
 		{
 			return false;
@@ -56,6 +61,7 @@ public abstract class EntityService<T, D> : GenericService<D>, IEntityService<T>
 
 	protected virtual bool ValidateUpdate(T data)
 	{
+		Log.Debug("Validating update: {data}", data);
 		if (data.Id <= 0)
 		{
 			return false;
@@ -65,11 +71,13 @@ public abstract class EntityService<T, D> : GenericService<D>, IEntityService<T>
 
 	public virtual bool Delete(int id)
 	{
+		Log.Debug("Deleting by id: {id}", id);
 		return _dataAccess.Delete(id);
 	}
 
 	public virtual List<T> GetAll()
 	{
+		Log.Debug("Getting all");
 		return _dataAccess.GetAll();
 	}
 }
