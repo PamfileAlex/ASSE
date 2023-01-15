e2e3449464fcb15b10af8344c0f02eab0e470adc
Pamfile Alex <damrosrak@gmail.com>
2023-01-15 21:01:59 +0200

    Added StyleCop Analyzer

--- ASSE.DomainModel/Models/UserRole.cs
+++ ASSE.DomainModel/Models/UserRole.cs
@@ -1,4 +1,6 @@
 ﻿using ASSE.Core.Models;
 
 namespace ASSE.DomainModel.Models;
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
 public readonly record struct UserRole(int UserId, int RoleId) : IRelationEntity;
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
